398cd3bc225954dff98dee73a55f5f91d818ef09
Sizan75 <sakiuzzaman26@gmail.com>
2022-09-29 04:09:46 +0600

    blogs component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Blogs from './component/Blogs/Blogs';
 import Exercises from './component/Exercises/Exercises';
 import Header from './component/Header/Header';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
    <Header></Header>
    <Exercises></Exercises>
+   <Blogs></Blogs>
     </div>
   );
 }
---
+++ src/component/Blogs/Blogs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div className='container mt-5'>
+        <h3>Question: How does React work?</h3>
+        <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props.It's possible to have as many components as necessary without cluttering your code.</p>    
+        </div>
+    );
+};
+
+export default Blogs;
