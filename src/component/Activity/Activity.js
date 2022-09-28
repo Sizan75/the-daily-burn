b949f9a15d15f4d2ed46192db221ede3928f03a1
Sizan75 <sakiuzzaman26@gmail.com>
2022-09-29 03:31:17 +0600

    activity part design completed

--- src/component/Activity/Activity.js
+++ src/component/Activity/Activity.js
@@ -3,26 +3,58 @@ import './Activity.css';
 import person from '../../image/people.jpg';
 
 const Activity = (props) => {
-    const {activities} = props;
+    const { activities } = props;
 
-    let time= 0 ;
-    for(const exercise of activities){
+    let time = 0;
+    for (const exercise of activities) {
         time = time + exercise.time;
-        console.log(exercise.time) 
+        console.log(exercise.time)
     }
     return (
         <div className='.bg-light  mt-5 pt-4 activity'>
             <div className='d-flex align-items-center justify-content-center mb-5'>
                 <img src={person} className='img-fluid person rounded-circle' alt="" />
-            <div className=''>
-                <h6>MD.SAKI-UZ-ZAMAN</h6>
-                <p>Dhaka,Bangladesh</p>
+                <div className=''>
+                    <h6>MD.SAKI-UZ-ZAMAN</h6>
+                    <p>Dhaka,Bangladesh</p>
+                </div>
             </div>
-            </div>
-            
-            <h5>Exercise Time: {time}</h5>
+            <div className='d-flex justify-content-between p-2 '>
 
-            <h5>Break Time:  </h5>
+                <div>
+                    <h4>65 <small>kg</small></h4>
+                    <p className='fw-semibold'>Weight</p>
+                </div>
+                <div>
+                    <h4>5.5</h4>
+                    <p className='fw-semibold'>Height</p>
+                </div>
+                <div>
+                    <h4>25 <small>yrs</small></h4>
+                    <p className='fw-semibold'>Age</p>
+                </div>
+            </div>
+
+            <div>
+                <h4 className='align-left mb-3'>Add A Break </h4>
+
+                <button className='rounded-circle btn btn-primary me-2'>10</button>
+                <button className='rounded-circle btn btn-primary me-2'>15</button>
+                <button className='rounded-circle btn btn-primary me-2'>20</button>
+                <button className='rounded-circle btn btn-primary me-2'>25</button>
+                <button className='rounded-circle btn btn-primary me-2'>30</button>
+            </div>
+
+
+
+            <div className='mt-5 align-left'>
+                <h5 className='mb-4'>Exercise Time: {time}</h5>
+
+                <h5 className='mb-4'>Break Time:  </h5>
+
+                <button className='btn btn-warning w-100'>Activity Completed</button>
+            </div>
+
 
         </div>
     );
--- src/component/Exercise/Exercise.js
+++ src/component/Exercise/Exercise.js
@@ -12,8 +12,8 @@ const Exercise = (props) => {
                 <img src={img}  alt="" />
                 <div className='card-body'>
                     <h4 className='card-title'>{name}</h4>
-                   <p><small>{details}</small></p>
-                    <p className='card-text fw-bold mb-2'>Time Required: {time} mins</p>
+                   <p className='align-left'><small >{details}</small></p>
+                    <p className='card-text fw-bold mb-2 align-left'>Time Required: {time} mins</p>
                 </div>
                 <button onClick={()=>handleAddToList(exercise)} className='btn btn-primary mt-auto w-100'>
                     Add to List
